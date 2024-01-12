87d36cceca47e21ffbd2e668932a2726591fa606
supsokman <supsokman@gmail.com>
2024-01-12 14:55:39 +0900

    초기페이지 SEO셋팅

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+
+const title = '숲속생활자: The Intuitive Vue Framework'
+const description = '숲속생활자 슬러건'
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description: description,
+  ogDescription: description,
+})
+</script>
+
 <template>
   <div>
     <h1>숲속생활자 메인</h1>
